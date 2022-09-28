5ec0ed50c00064b266f018f31eaf5230522ebaa1
KazakovDi <diman.ua68@gmail.com>
2022-09-28 21:42:09 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 })
-
+// "http://localhost:5000"
+//
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem("token")
     return config
