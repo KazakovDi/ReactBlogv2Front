ae615e43d9b6f1023a37927a89c3e2f1396903f3
KazakovDi <diman.ua68@gmail.com>
2022-12-16 02:08:04 +0200

    hosting changed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "https://nervous-school-uniform-duck.cyclic.app"
+    baseURL: "https://blogged.onrender.com"
 })
 // "http://localhost:5000"
 //https://nervous-school-uniform-duck.cyclic.app
